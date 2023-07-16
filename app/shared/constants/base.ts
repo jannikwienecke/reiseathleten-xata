c75224bd338d56dee6a73e380ae2fb30e4722a5f
jannikwienecke <wienecke.jannik@gmail.com>
2023-07-16 12:38:06 +0200

    production env

--- app/shared/constants/base.ts
+++ app/shared/constants/base.ts
@@ -3,12 +3,4 @@ export const MOCK_SERVER_URL = `http:/127.0.0.1:3001`;
 
 // TODO change this to the real api url
 
-const envIsProduction = process.env.NODE_ENV === "production";
-
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
-
-// check that when we build this IS_PRODUCTION is true
-
-if (envIsProduction && !IS_PRODUCTION) {
-  throw new Error("IS_PRODUCTION is false");
-}
